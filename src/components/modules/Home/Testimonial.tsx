872e341c4eb9a995a9c93df751f9d3e7a73cdb1c
ihrimon <ihrimon147@gmail.com>
2025-10-28 22:01:30 +0600

    update: review

--- src/components/modules/Home/Testimonial.tsx
+++ src/components/modules/Home/Testimonial.tsx
@@ -1,50 +1,19 @@
 'use client';
 
 import Carousel from '@/components/ui/carousel';
+import { categoryLinks } from '@/constants/category';
 export function Testimonial() {
-  const slideData = [
-    {
-      title: 'Mystic Mountains',
-      button: 'Explore Component',
-      src: '/images/review-1.jpg',
-    },
-    {
-      title: 'Urban Dreams',
-      button: 'Explore Component',
-      src: '/images/review-2.jpg',
-    },
-    {
-      title: 'Neon Nights',
-      button: 'Explore Component',
-      src: '/images/review-3.jpg',
-    },
-    {
-      title: 'Desert Whispers',
-      button: 'Explore Component',
-      src: '/images/review-4.jpg',
-    },
-    {
-      title: 'Desert Whispers',
-      button: 'Explore Component',
-      src: '/images/review-5.jpg',
-    },
-    {
-      title: 'Desert Whispers',
-      button: 'Explore Component',
-      src: '/images/review-6.jpg',
-    },
-    {
-      title: 'Desert Whispers',
-      button: 'Explore Component',
-      src: '/images/review-7.jpg',
-    },
-  ];
+  const slideData = categoryLinks.map((category, index) => ({
+    title: category.name,
+    button: `Explore ${category.name}`,
+    src: `/images/review-${(index % 7) + 1}.jpg`,
+  }));
   return (
     <div>
       <h2 className='text-4xl font-semibold mb-10 text-center'>
         Top Customer Reviews
       </h2>
-      <div className='relative overflow-hidden w-full h-full py-20'>
+      <div className='relative overflow-hidden w-full h-full pb-20'>
         <Carousel slides={slideData} />
       </div>
     </div>
